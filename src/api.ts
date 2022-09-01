fb36e164bd1cd66d33e7dabcf8dfc355a53f4d7d
Wade <wade.a.moment@gmail.com>
2022-09-01 11:29:22 -0700

    CRUD

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,79 @@
 import axios, { AxiosResponse } from "axios"
 
 const baseUrl: string = "http://localhost:4000"
+const getToDosUrl: string = baseUrl + "/todos"
+const addTodoUrl: string = baseUrl + "/add-todo"
 
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
     const todos: AxiosResponse<ApiDataType> = await axios.get(
-      baseUrl + "/todos"
+        getToDosUrl
     )
     return todos
   } catch (error: unknown) {
-    if (error instanceof Error) {
-        throw new Error(error.message)
-    } else {
-        throw new Error("Failed to get " + baseUrl.toString() + "/todos")
+    throw new Error(
+        (error instanceof Error) 
+        ? error.message 
+        : "Failed to getToDosUrl: " + getToDosUrl)
+  }
+}
+
+export const addTodo = async (
+    formData: ITodo
+  ): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+      const todo: Omit<ITodo, "_id"> = {
+        name: formData.name,
+        description: formData.description,
+        status: false,
+      }
+      const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
+        addTodoUrl,
+        todo
+      )
+      return saveTodo
+    } catch (error) {
+        throw new Error(
+            (error instanceof Error) 
+            ? error.message 
+            : "Failed to addTodoUrl: " + addTodoUrl)
+    }
+}
+
+export const updateTodo = async (
+    todo: ITodo
+  ): Promise<AxiosResponse<ApiDataType>> => {
+    const updateTodoUrl:string = `${baseUrl}/edit-todo/${todo._id}`;
+    try {
+      const todoUpdate: Pick<ITodo, "status"> = {
+        status: true,
+      }
+      const updatedTodo: AxiosResponse<ApiDataType> = await axios.put(
+        updateTodoUrl,
+        todoUpdate
+      )
+      return updatedTodo
+    } catch (error) {
+        throw new Error(
+            (error instanceof Error) 
+            ? error.message 
+            : "Failed to updateTodoUrl: " + updateTodoUrl)
     }
   }
+
+export const deleteTodo = async (
+    _id: string
+  ): Promise<AxiosResponse<ApiDataType>> => {
+    const deletedTodoUrl: string = `${baseUrl}/delete-todo/${_id}`
+    try {
+      const deletedTodo: AxiosResponse<ApiDataType> = await axios.delete(
+        deletedTodoUrl
+      )
+      return deletedTodo
+    } catch (error) {
+        throw new Error(
+            (error instanceof Error) 
+            ? error.message 
+            : "Failed to deletedTodoUrl: " + deletedTodoUrl)
+    }
 }
